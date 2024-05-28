4cd4e31ab8217b1417581bbb72504af032c44a8d
zsg <zhao15085@qq.com>
2024-05-28 23:43:06 +0800

    用户模块-数据库连接成功

--- user-service/src/main/java/com/goal/user/controller/AddressController.java
+++ user-service/src/main/java/com/goal/user/controller/AddressController.java
@@ -1,7 +1,24 @@
 package com.goal.user.controller;
 
+import com.goal.user.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
+@RequestMapping("api/${app.config.api.version}/addresses")
 public class AddressController {
+
+    @Resource
+    private UserService userService;
+
+    @GetMapping("{id}")
+    public Object detail(@PathVariable Long id) {
+        return userService.getById(id);
+    }
+
+
 }
