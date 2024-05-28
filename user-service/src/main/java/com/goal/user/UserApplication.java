af3a404f7f53b527a6e65a28627e474abc952f2f
zsg <zhao15085@qq.com>
2024-05-28 23:19:53 +0800

    用户模块-数据库配置

---
+++ user-service/src/main/java/com/goal/user/UserApplication.java
@@ -0,0 +1,15 @@
+package com.goal.user;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.goal.user.mapper")
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
