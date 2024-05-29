f3d5f8967c71369c69fe0d954bbc07fd3c1b08ed
zsg <zhao15085@qq.com>
2024-05-29 20:45:07 +0800

    接口文档配置-公共参数

--- shop-common/src/main/java/com/goal/config/SwaggerConfig.java
+++ shop-common/src/main/java/com/goal/config/SwaggerConfig.java
@@ -3,17 +3,23 @@ package com.goal.config;
 import lombok.Data;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import org.springframework.http.HttpMethod;
+import springfox.documentation.builders.*;
 import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.schema.ScalarType;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
+import springfox.documentation.service.Response;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @Configuration
-@EnableOpenApi
+@EnableOpenApi          // localhost:9001/swagger-ui/index.html
 public class SwaggerConfig {
 
     @Bean
@@ -28,6 +34,26 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.goal"))
                 .paths(PathSelectors.ant("/api/**"))
                 .paths(PathSelectors.any())
+                .build()
+                // 全局公共参数
+                .globalRequestParameters(globalRequestParameters())
+                .globalResponses(HttpMethod.GET, getGlobalResponseMessage())
+                .globalResponses(HttpMethod.POST, getGlobalResponseMessage());
+    }
+
+
+    @Bean
+    public Docket adminApiDoc() {
+
+        return new Docket(DocumentationType.OAS_30)
+                .groupName("管理端端接口文档")
+                .pathMapping("/")
+                .enable(true)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.goal"))
+                .paths(PathSelectors.ant("/admin/**"))
+                .paths(PathSelectors.any())
                 .build();
     }
 
@@ -38,4 +64,39 @@ public class SwaggerConfig {
                 .version("v1.0")
                 .build();
     }
+
+    /**
+     * 通用请求参数
+     * @return
+     */
+    private List<RequestParameter> globalRequestParameters() {
+
+        List<RequestParameter> parameters = new ArrayList<>();
+
+        parameters.add(new RequestParameterBuilder()
+                .name("token")
+                .description("登录令牌")
+                .in(ParameterType.HEADER)
+                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                .required(false)
+                .build());
+
+        return parameters;
+    }
+
+    /**
+     * 通用响应信息
+     * @return
+     */
+    private List<Response> getGlobalResponseMessage() {
+        List<Response> responseList = new ArrayList<>();
+
+        responseList.add(new ResponseBuilder()
+                .code("4xx")
+                .description("请求错误，请稍后再试")
+                .build());
+
+        return responseList;
+    }
+
 }
--- user-service/src/main/java/com/goal/user/controller/AddressController.java
+++ user-service/src/main/java/com/goal/user/controller/AddressController.java
@@ -1,7 +1,9 @@
 package com.goal.user.controller;
 
 import com.goal.user.service.AddressService;
-import com.goal.user.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@Api("收货地址服务")
 @RestController
 @RequestMapping("api/${app.config.api.version}/addresses")
 public class AddressController {
@@ -16,8 +19,11 @@ public class AddressController {
     @Resource
     private AddressService addressService;
 
+    @ApiOperation("根据id查找地址详情")
     @GetMapping("{id}")
-    public Object detail(@PathVariable Long id) {
+    public Object detail(
+            @ApiParam(value = "地址id", required = true)
+            @PathVariable Long id) {
         return addressService.getById(id);
     }
 
