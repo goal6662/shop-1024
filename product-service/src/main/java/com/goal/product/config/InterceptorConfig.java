99bfc778966c93fb50d226fa4bc2bd61e92bfb44
zsg <zhao15085@qq.com>
2024-06-04 19:58:15 +0800

    商品模块-拦截器配置

---
+++ product-service/src/main/java/com/goal/product/config/InterceptorConfig.java
@@ -0,0 +1,28 @@
+package com.goal.product.config;
+
+import com.goal.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LoginInterceptor loginInterceptor() {
+        return new LoginInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor())
+                .addPathPatterns("/api/*/product/**")
+                .excludePathPatterns(
+                        "/api/*/product/page_product",
+                        "/api/*/product/find/*"
+                );
+    }
+}
