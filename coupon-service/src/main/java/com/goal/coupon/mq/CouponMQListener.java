86b5acf7caaa2d36df64a6669fb3ce2ff8a59b38
zsg <zhao15085@qq.com>
2024-06-09 18:02:42 +0800

    优惠券模块-监听优惠券task消息

---
+++ coupon-service/src/main/java/com/goal/coupon/mq/CouponMQListener.java
@@ -0,0 +1,47 @@
+package com.goal.coupon.mq;
+
+import com.goal.coupon.service.CouponRecordService;
+import com.goal.domain.CouponRecordMessage;
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RabbitListener(queues = "${mq.config.coupon.release_queue}")
+public class CouponMQListener {
+
+    @Resource
+    private CouponRecordService couponRecordService;
+
+    @RabbitHandler
+    public void releaseCouponRecord(CouponRecordMessage couponRecordMessage,
+                                    Message message, Channel channel) throws IOException {
+
+        log.info("监听到消息：{}", couponRecordMessage);
+
+        long msgTag = message.getMessageProperties().getDeliveryTag();
+        boolean flag = couponRecordService.releaseCouponRecord(couponRecordMessage);
+
+        try {
+            if (flag) {
+                // 确认消息消费成功
+                channel.basicAck(msgTag, false);
+            } else {
+                log.error("释放优惠券失败：{}", couponRecordMessage);
+                channel.basicReject(msgTag, true);  // true 消费失败重新入队
+            }
+        } catch (Exception e) {
+            log.error("释放优惠券异常：{}", e.getMessage());
+            channel.basicReject(msgTag, true);  // true 消费失败重新入队
+        }
+
+    }
+
+}
