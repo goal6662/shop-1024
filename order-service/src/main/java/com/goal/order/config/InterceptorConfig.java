6dcaae324a5162fdf54818cafc381419a18bb7bd
zsg <zhao15085@qq.com>
2024-06-11 15:48:39 +0800

    feign远程调用token丢失解决

--- order-service/src/main/java/com/goal/order/config/InterceptorConfig.java
+++ order-service/src/main/java/com/goal/order/config/InterceptorConfig.java
@@ -1,10 +1,15 @@
 package com.goal.order.config;
 
 import com.goal.interceptor.LoginInterceptor;
+import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
@@ -22,6 +27,26 @@ public class InterceptorConfig implements WebMvcConfigurer {
                         "/api/*/callback/**",   // 回调接口
                         "/api/*/order/query_state/*"     // 定时任务：查询订单状态
                 );
+    }
+
+
+    @Bean
+    public RequestInterceptor userInfoRequestInterceptor() {
+        return requestTemplate -> {
+
+            // 从请求上下文获取请求
+            // 一个请求对应一个线程
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder
+                    .getRequestAttributes();
+
+            // 获取设置token
+            if (attributes != null) {
+                HttpServletRequest request = attributes.getRequest();
+                String token = request.getHeader("token");
+                requestTemplate.header("token", token);
+            }
+
+        };
     }
 
 }
